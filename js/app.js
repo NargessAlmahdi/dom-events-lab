6108f8f20641eac2a62f5b0633d0761c473287ca
NargessAlmahdi <nargess.almahdi2002@gmail.com>
2024-11-23 18:17:55 +0300

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,38 +1,104 @@
-// displayScreen
-// operationButton
-// equalsButton 
-// clearButton
+// // displayScreen
+// // operationButton
+// // equalsButton 
+// // clearButton
+
+// /*-------------------------------- Constants --------------------------------*/
+
+
+// /*-------------------------------- Variables --------------------------------*/
+// // let oneInput = ''
+// // let twoInput = ''
+// // let operator = ''
+// let displayScreen = ''
+// /*------------------------ Cached Element References ------------------------*/
+
+
+// // buttons
+// const numberButton = document.querySelectorAll('.number')
+// const operationButton = document.querySelectorAll('.operator')
+// const equalsButton = document.querySelector('.equals')
+// const displayScreenElement = document.querySelector ('.display')
+
+
+
+// /*----------------------------- Event Listeners -----------------------------*/
+// numberButton.forEach(button => {
+//     button.addEventListener('click', () => {
+//         displayScreen += button.textContent
+//         displayScreenElement.textContent = displayScreen
+
+//     })
+// })
+
+// /*-------------------------------- Functions --------------------------------*/
+
+
+
 
 /*-------------------------------- Constants --------------------------------*/
-
-
 /*-------------------------------- Variables --------------------------------*/
-// let oneInput = ''
-// let twoInput = ''
-// let operator = ''
-let displayScreen = ''
+let firstNumber = '';
+let secondNumber = '';
+let operator = null;
+let result = 0;
 /*------------------------ Cached Element References ------------------------*/
-
-
-// buttons
-const numberButton = document.querySelectorAll('.number')
-const operationButton = document.querySelectorAll('.operator')
-const equalsButton = document.querySelector('.equals')
-const displayScreenElement = document.querySelector ('.display')
-
-
-
+const buttons = document.querySelectorAll('.button')
+const displayEl = document.querySelector('.display')
 /*----------------------------- Event Listeners -----------------------------*/
-numberButton.forEach(button => {
-    button.addEventListener('click', () => {
-        displayScreen += button.textContent
-        displayScreenElement.textContent = displayScreen
-
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        // if it is a number
+        if(event.target.classList.contains("number")) {
+            if(operator === null) {
+                firstNumber += event.target.innerText
+                displayEl.innerText = firstNumber
+            } else {
+                secondNumber += event.target.innerText
+                displayEl.innerText = firstNumber + operator + secondNumber 
+            }
+        }
+        // if it is an operator
+        else if (event.target.classList.contains("operator")){
+            let selectedOperator = event.target.innerText
+            if (selectedOperator === 'C') {
+              // CLEAR DISPLAY
+              displayEl.innerText = ''
+              firstNumber = ''
+              secondNumber = ''
+              operator = null
+              result = 0
+            } else {
+                if (firstNumber !== '') {
+                    operator = event.target.innerText   
+                    displayEl.innerText = firstNumber + operator
+                }
+            }
+            
+        } 
+        // if it's equals
+        
+        else if (event.target.classList.contains("equals")) {
+            calculate()
+        }
     })
 })
-
-
-
-
-
 /*-------------------------------- Functions --------------------------------*/
+function calculate() {
+    if(firstNumber !== '' && secondNumber !== '' && operator !== null) {
+        let num1 = parseFloat(firstNumber)
+        let num2 = parseFloat(secondNumber)
+        // do the calculation
+        if (operator === '+') {
+            result = num1 + num2
+        } else if(operator === '-') {
+            result = num1 - num2
+        } else if(operator === '*') {
+            result = num1 * num2
+        } else if(operator === '/') {
+            result = num1 / num2
+        }
+        displayEl.innerText = result
+    }
+}
+    
